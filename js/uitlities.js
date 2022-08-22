ca0d6de8b97ae1488dc68d88796a103a671d3375
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-08-22 16:23:04 +0600

    Select 5 section done

---
+++ js/uitlities.js
@@ -0,0 +1,41 @@
+// players array
+const playerNames = [];
+
+//update player list display function
+function displayPlayer(playerNames) {
+  // acces the player list
+  const playerList = document.getElementById("playerList");
+  playerList.innerHTML = "";
+  // loop through array of players
+  for (let i = 0; i < playerNames.length; i++) {
+    const name = playerNames[i];
+
+    // each iteration list create and add conditions
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = ` 
+    <tr>
+      <th>${i + 1 + " ."}</th>
+      <td>   ${name}</td>
+    </tr>`;
+    playerList.appendChild(tr);
+  }
+}
+
+// selecting player function
+
+function addPlayers(element) {
+  const playerName = element.parentNode.parentNode.children[0].innerText;
+
+  const playerNameObj = {
+    playerName: playerName,
+  };
+  // up-to 5 players selection condition and show alert im more than 5
+  if (playerNames.length <= 4) {
+    playerNames.push(playerName);
+  } else {
+    alert("You are not allowed to add more than 5 Players");
+  }
+  //  update player list passed to the display function
+  displayPlayer(playerNames);
+}
